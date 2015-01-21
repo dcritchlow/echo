fbd852efff4781e5a5e2dbbd2ce0d6fe73ea8fc1
Darin Critchlow <darincritchlow@gmail.com>
2015-01-21 16:15:38 +0000

    Assignment completed

--- php/echo.php
+++ php/echo.php
@@ -1,10 +1,15 @@
+See console.
 <?php
 
-function echo()
+function echo_to_console()
 {
-  // ...
+  for( $i = 0; $i < func_num_args(); $i++ )
+  {
+    $argument = func_get_arg($i);
+    echo "<script>console.log('$argument')</script>";
+  }
 }
 
-echo();
-echo('bla');
-echo('foo', 'bar', 'baz');
+echo_to_console();
+echo_to_console('bla');
+echo_to_console('foo', 'bar', 'baz');
